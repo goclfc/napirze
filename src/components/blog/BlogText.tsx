bb163fb12160271053c867de8c907fa89e27ff81
goclfc <gocha.berulava@gmail.com>
2024-10-11 02:19:04 +0400

    posts

--- src/components/blog/BlogText.tsx
+++ src/components/blog/BlogText.tsx
@@ -1,16 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react";
 
 type Props = {
-    text:string
-}
+  text: string;
+};
 
 const BlogText = (props: Props) => {
-useEffect(()=>{
-const item = document.getElementById("thismf")
-item!.innerHTML += props.text
-},[props.text])
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    ref.current!.innerHTML = props.text;
+  }, [props.text]);
   return (
-    <div className='font-smooch-bold w-full md:w-1/2 mx-auto py-2 px-1 text-2xl text-bold text-justify' id='thismf'></div>
-  )
-}
-export default BlogText
+    <div
+      className="font-smooch-bold w-full md:w-1/2 mx-auto py-2 px-1 text-2xl text-bold text-justify"
+      ref={ref}
+    ></div>
+  );
+};
+export default BlogText;
